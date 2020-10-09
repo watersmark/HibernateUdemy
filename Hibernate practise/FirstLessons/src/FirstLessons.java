fbced766abd96d4b828adbbe3a6693db9bacbadc
watersmark <DuldinEV@mail.ru>
2020-10-10 00:57:24 +0300

    Add all

---
+++ Hibernate practise/FirstLessons/src/FirstLessons.java
@@ -0,0 +1,6 @@
+public class FirstLessons {
+    public static void main(String[] args) {
+
+        
+    }
+}
---
+++ Hibernate practise/MavenHibernate/src/main/java/mains.java
@@ -0,0 +1,8 @@
+public class mains {
+    public static void main(String[] args) {
+
+        System.out.println("GG");
+
+
+    }
+}
